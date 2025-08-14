ded0a8aa970e5d75531734e6802d36c8752b0ac2
Anaïs Betts <anais@anaisbetts.org>
2025-08-15 01:02:39 +0200

    Whateverrrr

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
+#!/usr/bin/env node
+
 import { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js'
 import { StdioServerTransport } from '@modelcontextprotocol/sdk/server/stdio.js'
 import { config } from 'dotenv'
 import * as pkg from '../package.json'
-import { createPaperlessClient } from './api-client'
+//import { createPaperlessClient } from './api-client'
 import { createServer } from './server'
 
 // Load environment variables
